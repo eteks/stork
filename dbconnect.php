912a29d5a3077a748d2844ea317eb85c6ce9ad33
spmuthu21 <muthu@globalensolutions.com>
2016-06-07 18:27:19 +0530

    initial commit by muthu

---
+++ dbconnect.php
@@ -0,0 +1,20 @@
+<?php
+class dbConnect {
+    public $DB_HOST='localhost';
+    public $DB_USER='root';
+    public $DB_PASSWORD='';
+    public $DB_DATABASE='wellocity';
+	function __construct() {
+        $conn = mysql_connect($this->DB_HOST, $this->DB_USER, $this->DB_PASSWORD);
+        mysql_select_db($this->DB_DATABASE, $conn);
+        if(!$conn){
+            die ("Cannot connect to the database");
+        }
+        return $conn;
+    }
+    public function Close(){
+        mysql_close();
+    }
+}
+$db = new dbConnect();
+?>
