0a47b70d7e9866b1750cb67e7788182381acaccb
akiladevi11 <akiladevi@etekchnoservices.com>
2016-06-16 19:56:08 +0530

    Added checkout.php & updated custom.css by Akila.

---
+++ checkout.php
@@ -0,0 +1,468 @@
+<?php include('header.php') ?>
+<main class="main">
+	<section class="header-page">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3 hidden-xs">
+						<h1 class="mh-title">Checkout</h1>
+					</div>
+					<div class="breadcrumb-w col-sm-9">
+						<span class="hidden-xs">You are here:</span>
+						<ul class="breadcrumb">
+							<li>
+								<a href="home.php">Home</a>
+							</li>
+							<li>
+								<span>Checkout</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			 </div>
+	</section> <!---breadcrumb------><!---section3--->
+  <div class="">	
+	<section id="checkout" class="pr-main">
+	 <div class="container">
+	  <div class="onepage">	
+	  <div class="cart-view-top">
+		 <div class="col-md-6 col-sm-6 col-xs-12">
+			<h1>Review Your Order here..</h1>
+		 </div>
+		 <div class="col-md-6 col-sm-6 col-xs-12 right">
+		    <h1>Continue Shopping</h1>
+		 </div>
+       </div>
+	  <br>	
+	  <div class="order-box">
+	   <!---order detail 1 starts--->			
+	   <div id="checkfull" class="col-md-8 col-sm-12 col-xs-12">
+	     <div  class="col-md-12 col-sm-12 col-xs-12" >
+	      <!-- render layout -->
+	      <fieldset class="round-box" id="cart-contents">
+	      <h3 class="title"><span class="icon fa fa-check"></span>REVIEW ORDER 1</h3>
+		  <table cellspacing="0" cellpadding="0" border="0" class="cart-summary no-border">
+			<tbody>
+			    <tr clas="top-header">
+					<th width="10%" align="left" class="th-price">Print Type</th>
+					<th width="10%" class="th-price">Print side</th>
+					<th width="10%" class="th-tax"><span class="priceColor2">Paper Size</span></th>
+					<th width="15%"  class="th-quanlity">Paper Type</th>
+					<th width="15%"  class="th-quanlity">Upload file pages no.</th>
+					<th width="15%"  class="th-quanlity">Total no. of pages</th>
+					<th width="15%" class="th-discount"><span class="priceColor2">Quantity</span></th>
+					<th width="15%" class="th-discount"><span class="priceColor2">Comments</span></th>
+					<th width="15%" class="th-discount"><span class="priceColor2">Total Cost</span></th>
+					<th width="15%" align="left" class="th-total th-last">SubTotal</th>
+			    </tr>
+	            <tr valign="top" id="product_row_0" class="product-detail-row product-detail-last-row sectiontableentry1">
+				  <td align="left" class="base-price">
+				  	<div class="PricebasePrice vm-display vm-price-value">
+						 <span class="vm-price-desc"></span>
+						 <span class="PricebasePrice">Black &amp; White</span>
+					</div> 	 
+				  </td>
+				  <td align="left" class="base-price">
+					<div class="PricebasePrice vm-display vm-price-value">
+						<span class="vm-price-desc"></span>
+						<span class="PricebasePrice">Double</span>
+					</div>			
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">A4</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">Bond Sheet</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">1-5,14-21,25-40</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">26</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="left" class="product-quanlity">
+				    <input type="text" id="quantity_0" value="2" maxlength="4" size="3" name="quantity[0]" class="quantity-input js-recalculate" title="Update Quantity In Cart">
+				  </td>
+				  <td>
+				  	<input type="text" id="quantity_0" maxlength="75" size="20" name="user-comment" class="" title="Update your comments here">
+				  </td>
+				  <td align="left" class="base-price">
+					<div class="PricebasePrice vm-display vm-price-value">
+						<span class="vm-price-desc"></span>
+						<span class="PricebasePrice">Rs.480</span>
+					</div>			
+				  </td>
+	              <td align="right" id="subtotal_with_tax_0" colspan="0" class="sub-total td-last"><span class="line-through">Rs.950</span></td>
+			    </tr>
+			   </tbody>
+		      </table>
+	        </fieldset>
+	      </div>
+	     </div> <!---order 1--->  
+	    <!---order detail 2 starts--->
+	    <div id="checkfull" class="col-md-8 col-sm-12 col-xs-12">
+	     <div  class="col-md-12 col-sm-12 col-xs-12" >
+	      <!-- render layout -->
+	      <fieldset class="round-box" id="cart-contents">
+	      <h3 class="title"><span class="icon fa fa-check"></span>REVIEW ORDER 2</h3>
+		  <table cellspacing="0" cellpadding="0" border="0" class="cart-summary no-border">
+			<tbody>
+			    <tr clas="top-header">
+					<th width="10%" align="left" class="th-price">Print Type</th>
+					<th width="10%" class="th-price">Print side</th>
+					<th width="10%" class="th-tax"><span class="priceColor2">Paper Size</span></th>
+					<th width="15%"  class="th-quanlity">Paper Type</th>
+					<th width="15%"  class="th-quanlity">Upload file pages no.</th>
+					<th width="15%"  class="th-quanlity">Total no. of pages</th>
+					<th width="15%" class="th-discount"><span class="priceColor2">Quantity</span></th>
+					<th width="15%" class="th-discount"><span class="priceColor2">Comments</span></th>
+					<th width="15%" class="th-discount"><span class="priceColor2">Total Cost</span></th>
+					<th width="15%" align="left" class="th-total th-last">SubTotal</th>
+			    </tr>
+	            <tr valign="top" id="product_row_0" class="product-detail-row product-detail-last-row sectiontableentry1">
+				  <td align="left" class="base-price">
+				  	<div class="PricebasePrice vm-display vm-price-value">
+						 <span class="vm-price-desc"></span>
+						 <span class="PricebasePrice">Color</span>
+					</div> 	 
+				  </td>
+				  <td align="left" class="base-price">
+					<div class="PricebasePrice vm-display vm-price-value">
+						<span class="vm-price-desc"></span>
+						<span class="PricebasePrice">Double</span>
+					</div>			
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">A4</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">Bond Sheet</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">1-5,50-80</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="right" class="pro_tax">
+				  	<span class="priceColor2">
+				  	 <div class="PricetaxAmount vm-display vm-price-value">
+				  		<span class="vm-price-desc"></span>
+				  		<span class="PricetaxAmount">35</span>
+				  	  </div>
+				  	 </span>
+				  </td>
+				  <td align="left" class="product-quanlity">
+				    <input type="text" id="quantity_0" value="1" maxlength="4" size="3" name="quantity[0]" class="quantity-input js-recalculate" title="Update Quantity In Cart">
+				  </td>
+				  <td>
+				  	<input type="text" id="quantity_0" maxlength="75" size="20" name="user-comment" class="" title="Update your comments here">
+				  </td>
+				  <td align="left" class="base-price">
+					<div class="PricebasePrice vm-display vm-price-value">
+						<span class="vm-price-desc"></span>
+						<span class="PricebasePrice">Rs.530</span>
+					</div>			
+				  </td>
+	              <td align="right" id="subtotal_with_tax_0" colspan="0" class="sub-total td-last"><span class="line-through">Rs.1480</span></td>
+			    </tr>
+			   </tbody>
+		      </table>
+	        </fieldset>
+	      </div>
+	    </div> <!----order 2---->
+	   </div><!---Order-box---> 
+	   <div class="clearfix"> </div>
+	   <br> 
+	   <div class="cart-view-top">
+		 <div class="col-md-6 col-sm-6 col-xs-12">
+			<h1>Shopping Cart</h1>
+		 </div>
+		 <div id="login-pane" class="col-md-12 col-sm-12 col-xs-12">
+             <p>Please fill in the fields below to complete your order.</p>
+         </div>
+	   </div>
+	   </hr>	
+       <div class=""> <!---Shipping details----> 
+       	 <!---shipping detail1 starts--->
+  		 <div  class="col-md-4 col-sm-6 col-xs-12">
+		   <div id="div_billto">
+			 <div class="pane round-box">
+			   <input type="checkbox" id="register"><label class="registers">Send to Personnel Address</label>	
+			   <h3 class="title"><span class="icon fa fa-check"></span>Shipping Detail 1</h3>
+				<div class="pane-inner">
+				 <br>	
+				  <ul id="table_billto" class="adminform user-details no-border">
+				  	<li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Name<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Address 1<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Address 2<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Area<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">City<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Postal Code<em>*</em></label>
+						<br>
+						<input type="text" maxlength="32" class="required" value="" size="30" name="zip" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">State<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+  					 <li class="short">
+                      <div class="field-wrapper">
+						<label for="phone_1_field" class="phone_1">Phone</label>
+						<br>
+						<input type="text" maxlength="32" value="" size="30" name="phone_1" id="phone_1_field"> 
+					  </div>
+					 </li>
+   					 <li class="short right">
+					  <div class="field-wrapper">
+     					<label for="phone_2_field" class="phone_2">	Mobile phone<em>*</em></label>
+						<br>
+						<input type="text" maxlength="32" value="" size="30" name="phone_2" id="phone_2_field"> 
+					  </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="email_field" class="address_1">E-Mail<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+				  </ul>
+				  <br>
+				  <input type="checkbox" id="register"><label class="registers">Make this as my default shipping address</label>
+				</div>
+	 		</div>
+		   </div>  <!-- billto -->
+         </div>
+         <!---shipping detail2 starts--->
+         <div  class="col-md-4 col-sm-6 col-xs-12 fl">
+		   <div id="div_billto">
+			 <div class="pane round-box">
+			  <input type="checkbox" id="register"><label class="registers">Send to College Address</label>	
+			   <h3 class="title"><span class="icon fa fa-check"></span>Shipping Detail 2</h3>
+				<div class="pane-inner">
+				 <br>	
+				  <ul id="table_billto" class="adminform user-details no-border">
+				  	<li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Student Name<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">ID No.<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Year of Studying<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Department<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">College Name<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Area<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">Postal Code<em>*</em></label>
+						<br>
+						<input type="text" maxlength="32" class="required" value="" size="30" name="zip" id="address_1_field"> 
+					   </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="address_1_field" class="address_1">State<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+  					 <li class="short">
+                      <div class="field-wrapper">
+						<label for="phone_1_field" class="phone_1">Phone</label>
+						<br>
+						<input type="text" maxlength="32" value="" size="30" name="phone_1" id="phone_1_field"> 
+					  </div>
+					 </li>
+   					 <li class="short right">
+					  <div class="field-wrapper">
+     					<label for="phone_2_field" class="phone_2">	Mobile phone<em>*</em></label>
+						<br>
+						<input type="text" maxlength="32" value="" size="30" name="phone_2" id="phone_2_field"> 
+					  </div>
+					 </li>
+					 <li class="long">
+					  <div class="field-wrapper">
+						<label for="email_field" class="address_1">E-Mail<em>*</em></label>
+						<br>
+						<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="address_1_field"> 
+					   </div>
+					 </li>
+				  </ul>
+				</div>
+	 		</div>
+		   </div>  <!-- billto -->
+         </div>
+      </div> <!---shipping details --->
+      <div class="clearfix"> </div>
+      <!--cost details---->
+      <div class="cart-view-top">
+		 <div class="col-md-6 col-sm-6 col-xs-12">
+			<h1>Costing Details</h1>
+		 </div>
+		 <div id="login-pane" class="col-md-12 col-sm-12 col-xs-12">
+             <p>Please check your total costs here.</p>
+         </div>
+         <div id="checkfull" class="col-md-8 col-sm-12 col-xs-12">
+          <div  class="col-md-12 col-sm-12 col-xs-12" >
+           <!-- render layout -->
+           <fieldset class="round-box" id="cart-contents">
+           <h3 class="title"><span class="icon fa fa-check"></span>Estimated Cost</h3>
+	       <table cellspacing="0" cellpadding="0" border="0" class="cart-summary no-border">
+		     <tbody>
+               <tr class="pr-total">
+		          <td colspan="6">
+			       <table>                             
+					 <tbody>
+						<tr class="first">
+							<td>SubTotal:</td>
+							<td class="pr-right"><div class="PricesalesPrice vm-display vm-price-value"><span class="vm-price-desc"></span><span class="PricesalesPrice">Rs2400.00</span></div></td>
+						</tr>
+						<tr style="display:none;">
+							<td>Discount:</td>
+							<td class="pr-right">
+							  <span id="total_amount" class="priceColor2">Rs30.00</span>                           
+							</td>
+						</tr>
+						<tr style="display:none;">
+							<td>Tax:</td>
+							<td class="pr-right"><span id="total_tax" class="priceColor2">Rs11.17</span></td>
+						</tr>
+						<tr style="display:none;">
+							<td>Shipment:</td>
+							<td class="pr-right"><span id="shipment" class="priceColor2">0</span></td>
+						</tr>
+														<tr class="last">
+							<td>Total:</td>
+							<td class="pr-right"><strong id="bill_total">Rs2400.00</strong></td>
+						</tr>
+					  </tbody>
+			        </table>
+			       </td>
+		          </tr>
+		        </tbody>
+		      </table>
+		      </fieldset> 
+		    </div>
+		  </div>   
+	   </div> <!---Cost details---->
+	   <!---button holder------>
+	   <div class="button_holder">
+			<h4 class="btn_prf"><a href="home.php">Sign up</a></h4>
+			<!--<h4 class="btn_prf"><a href="home.html">Reset</a></h4>-->
+			<h4 class="btn_prf"><a href="home.php">Sign in</a></h4>
+			<h4 class="btn_prf"><a href="home.php">Login with Facebooko</a></h4>
+			<h4 class="btn_prf"><a href="home.php">Login with Google+</a></h4><br>
+	   </div>
+	   <div class="clearfix">  </div>
+	   <div class="button_holder btn_pay">
+	   		<h4 class="btn_prf"><a href="home.php">Pay Now</a></h4>
+			<h4 class="btn_prf"><a href="home.php">Clear</a></h4>
+	    </div>		
+  </div> <!--onepage--->
+  </div> <!--container---->
+ </section>
+</div>
+
+</main><!--Main index : End-->
+
+<?php include('footer.php') ?>
