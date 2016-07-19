21a207be62c80cfe4945fba236cfd7cf3e169a0b
spmuthu21 <muthu@globalensolutions.com>
2016-07-19 18:46:49 +0530

    implement cabin_booking by muthukaruppan

--- cabin_booking.php
+++ cabin_booking.php
@@ -26,19 +26,6 @@
  	}
 ?>
 	
-	<script>
-	function getSlot(val) {
-		$.ajax({
-			type: "POST",
-			url: "cabin_query.php",
-			data:{ requiredate : val },
-			success: function(data){
-				$("#required_date").val(val);
-			}
-		});
-	}
-	</script>
-
       <section id="checkout" class="pr-main">
 			<div class="container">	
 			  <div class="cart-view-top">
@@ -51,28 +38,36 @@
 				 <div id="div_billto">
 					   <div class="pane round-box">
 						 <h3 class="title"><span class="icon icon-one">1</span>Book Your Cabin Now</h3>
-						 <form id="cabin_booking" class="cabin_booking" name="cabin_booking" method="post" action="">
+						 <form id="cabin_booking" class="cabin_booking" name="cabin_booking" method="post" action="http://www.printstork.com/ccavRequestHandler.php">
+						 	<input type="hidden" name="tid" id="txnid" readonly />
+		   					<input type="hidden" name="merchant_id" value="<?php echo MERCHANTID; ?>"/>
+		   					<input type="hidden" name="order_id" value="<?php echo 'cab_'.$_SESSION['session_id']; ?>"/>
+		   					<input type="hidden" name="currency" value="INR"/>
+					   		<input type="hidden" name="redirect_url" value="<?php echo CCAVENUEREDIRECTURL; ?>"/>
+					   		<input type="hidden" name="cancel_url" value="<?php echo CCAVENUECANCELURL; ?>"/>
+					   		<input type="hidden" name="language" value="EN"/>
+					   		<input type="hidden" name="billing_country" value="India"/>
 						  <div class="pane-inner">
 							 <ul id="table_billto" class="adminform user-details no-border">
 				  				<li class="long">
 					  			  <div class="field-wrapper">
 									<label for="address_1_field" class="address_1">Name<em>*</em></label>
 									<br> 
-									<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="cabin_name"> 
+									<input type="text" maxlength="64" class="required" value="" size="30" name="billing_name" id="cabin_name"> 
 					   			  </div>
 					            </li> 
 					            <li class="long">
 					  			  <div class="field-wrapper">
      					             <label for="phone_2_field" class="phone_2">Mobile phone<em>*</em></label>
 						             <br>
-						             <input type="text" maxlength="10" value="" size="30" name="phone_2" id="cabin_mobile"> 
+						             <input type="text" maxlength="10" value="" size="30" name="billing_tel" id="cabin_mobile"> 
 					              </div>
 					            </li> 
 					            <li class="long">
 					  				<div class="field-wrapper">
 										<label for="address_1_field" class="address_1">Timing Type<em>*</em></label>
 										<br>
-										<select class="select_time" id="cabin_timing_type" onChange="getSlot(this.value);">
+										<select class="select_time" id="cabin_timing_type" name="merchant_param1">
 											<option val=""> Select Timing Type </option>
 										<?php
 											foreach($timing_type as $key => $value){
@@ -95,32 +90,35 @@
 			                      <div class="field-wrapper">
 									<label for="txtDate" class="phone_1">Required Date</label>
 									<br>
-									<input type="text" id="cabin_booking_date" maxlength="32" value="" size="30" name="require_date" id="txtDate"> 
+									<input type="text" id="cabin_booking_date" maxlength="32" value="" size="30" name="merchant_param2" id="txtDate"> 
 								  </div>
 					 			</li>
 					 			<li class="long num_of_system_restiction">
 								  <div class="field-wrapper">
 									<label for="address_1_field" class="address_1">No. of System<em>*</em></label>
 									<br>
-									<input type="text" maxlength="2" class="required" value="" size="30" name="zip" id="cabin_required_system"> 
+									<input type="text" maxlength="2" class="required" value="" size="30" name="zip" name="merchant_param3" id="cabin_required_system">
+									<input type="hidden" class="maximum_booking_system" value="0">
 								   </div>
 					 			</li>
 					    		<li class="long">
 			                      <div class="field-wrapper">
 									<label for="phone_1_field" class="phone_1">Total Hours<em>*</em></label>
 									<br>
-									<input type="text" maxlength="32" value="" size="30" name="phone_1" id="phone_1_field" readonly> 
+									<input type="text" maxlength="32" value="" size="30" name="merchant_param4" id="phone_1_field" class="total_hours_cabin_booking_view"  readonly>
+									<input type="hidden" class="total_hours_cabin_booking" value="0"> 
 								  </div>
 					 			</li>
    					            <li class="long">
 								  <div class="field-wrapper">
 									<label for="email_field" class="address_1">Total Cost</label>
 									<br>
-									<input type="text" maxlength="64" class="required" value="" size="30" name="address_1" id="" readonly> 
+									<input type="text" maxlength="64" class="required cabin_booking_total_amount" value="0" size="30" name="amount" id="" readonly> 
 								   </div>
 					 			</li>
 					 			<span class="details-button">
-									<input id="cabin_book_button" class="details-button" type="button" title="Book" value="Book">
+					 				<input type="hidden" name="merchant_param5" class="schedule_time_id" value="">
+									<input id="cabin_book_button" class="details-button" type="submit" title="Book" value="Book">
 									<input id="cabin_clear_button" class="details-button" type="button" title="Clear" value="Clear">
 							    </span> 
 				 			   	<div> 
@@ -166,3 +164,9 @@
 <?php 
 	include('footer.php');
 ?>
+<script>
+	$(window).load(function(){
+		var d = new Date().getTime();
+		$('#txnid').val(d);
+	});
+</script>
